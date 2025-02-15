d821e7c5bcf12eb424de219d84a2c06ebf3999e4
maityamit007 <mitwaamit007@gmail.com>
2025-02-15 17:06:56 +0530

    KPI Tracker

--- components/kpi.tsx
+++ components/kpi.tsx
@@ -34,6 +34,20 @@ const Kpi: React.FC<KpiProps> = ({ data = [], story, chartData }) => {
           <StatBox key={index} value={stat.value} title={stat.title} />
         ))}
       </div>
+      <div className="w-full flex flex-col items-start justify-start">
+        <h2 className="text-lg font-semibold mb-5 text-gray-800">Bussiness Questions</h2>
+        <div className="flex flex-wrap w-full gap-2">
+          {[{ name: 'Question One', description: 'Short Description Goes Here' },
+          { name: 'Question Two', description: 'Short Description Goes Here' },
+          { name: 'Question Three', description: 'Short Description Goes Here' },
+          { name: 'Question Four', description: 'Short Description Goes Here' },
+          ].map((asset) => (
+            <div className='flex flex-col cursor-pointer hover:bg-gray-200 w-[18.75rem] items-start gap-1 p-4 bg-card border rounded-lg shadow-sm'>
+              <h2 className="text-sm font-semibold text-gray-800">{asset.name}</h2>
+              <p className="text-sm text-gray-900 mt-1">{asset.description}</p>
+            </div>))}
+        </div>
+      </div>
     </div>
   );
 };
